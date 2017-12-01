17beddaa8147b2343485a0a032c3587ec07d0280
Brandon Kudo <brandonkudo@gmail.com>
2017-12-01 15:58:45 -0800

    get one response

--- js/API-interface.js
+++ js/API-interface.js
@@ -1,22 +1,28 @@
-import { DoctorAPI } from "./../js/API.js";
+// import { Doctor } from "./../js/API.js";
 var apiKey = require('./../.env').apiKey;
+// var newDoctor = new Doctor();
 
-function showDoctor(doctor) {
-  $("#doctor-name").text(doctor);
-}
-
-function error() {
-  $("#error").text("Uh oh an error occurred");
-}
+// function showDoctor(doctor) {
+//   $("#doctor-name").text(doctor);
+// }
+//
+// function error() {
+//   $("#error").text("Uh oh an error occurred");
+// }
 
 
 $(document).ready(function() {
   $("#results").hide();
-  $("#form").submit(function(event) {
-    event.preventDefault();
-    $("#results").show();
-    var newAPI = new API();
+  $("#ailment-button").click(function() {
     let input = $("#ailment").val();
-    newAPI.getDoctors(input)
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5231,-122.6765,100&user_location=45.5231,-122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response){
+      $("#results").show();
+      for (var i = 0; i < response.data.length; i++) {
+      let doctor = `${response.data[i].profile.first_name} ${response.data[i].profile.last_name}`;
+      $("#doctor-name").text(doctor);
+      }
+    }).fail(function(error) {
+      $("#error").text("Uh oh an error occurred");
+    });
   });
 });
