97bbd9233dee115cdd7bea16a03325cf5090b416
Brandon Kudo <brandonkudo@gmail.com>
2017-12-01 19:07:22 -0800

    make second click function and refactor

--- js/API-interface.js
+++ js/API-interface.js
@@ -1,47 +1,78 @@
-// import { Doctor } from "./../js/API.js";
 var apiKey = require('./../.env').apiKey;
-// var newDoctor = new Doctor();
 
-// function showDoctor(doctor) {
-//   $("#doctor-name").text(doctor);
-// }
-//
-// function error() {
-//   $("#error").text("Uh oh an error occurred");
-// }
 
 
 $(document).ready(function() {
-  $("#results").hide();
+  // $("#output").hide();
   $("#ailment-button").click(function() {
     let input = $("#ailment").val();
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5231,-122.6765,100&user_location=45.5231,-122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response){
       $("#output").show();
       if(response.data.length > 0) {
       for (var i = 0; i < response.data.length; i++) {
-        let doctor = `${response.data[i].profile.first_name} ${response.data[i].profile.last_name}`;
-        $("#output").append(`${doctor}<br>`);
-        for (var j = 0; j < response.data[i].practices.length; j++) {
-          let practice = response.data[i].practices[j];
-          $("#output").append(`${practice.visit_address.street}<br>${practice.visit_address.city}, ${practice.visit_address.state}<br>`);
-          $("#output").append(`${practice.phones[0].number}<br>`);
-          if(practice.website === undefined) {
-            $("#output").append(`This doctor does not have a website<br>`);
+        let index = response.data[i];
+        let profiles = index.profile;
+        $("#output").append(`<p class="name">${profiles.first_name} ${profiles.last_name}</p>`);
+        for (var j = 0; j < index.practices.length; j++) {
+          let practice = index.practices[j];
+          let address = practice.visit_address;
+          let site = practice.website;
+          $("#output").append(`<p class="address">${address.street}</p>`);
+          $("#output").append(`<p class="city">${address.city}, ${address.state}</p>`)
+          $("#output").append(`<p class="phone">${practice.phones[0].number}</p>`);
+          if(site === undefined) {
+            $("#output").append(`<p class="website">This doctor does not have a website</p>`);
           } else {
-            $("#output").append(`${practice.website}<br>`);
+            $("#output").append(`<a href="${site}" class="website">${site}</a>`);
           }
           if(practice.accepts_new_patients === true) {
-            $("#output").append(`Now accepting new patients<br>`);
+            $("#output").append(`<p class="new-patient">Now accepting new patients</p>`);
           } else {
-            $("#output").append(`Not accepting new patients<br>`);
+            $("#output").append(`<p class="new-patient">Not accepting new patients</p>`);
           }
         }
       }
       } else {
-        $("#doctor-name").append("No doctors in the area fit match that inquiry.")
+        $("#output").append(`No doctors in the area fit match that inquiry.`)
       }
     }).fail(function(error) {
-      $("#error").text("Uh oh an error occurred");
+      $("#output").text("Uh oh an error occurred");
+    });
+  });
+
+  $("#name-button").click(function() {
+    let input = $("#name").val();
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${input}&location=45.5231,-122.6765,100&user_location=45.5231,-122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response){
+      $("#output").show();
+      if(response.data.length > 0) {
+      for (var i = 0; i < response.data.length; i++) {
+        let index = response.data[i];
+        let profiles = index.profile;
+        $("#output").append(`<p class="name">${profiles.first_name} ${profiles.last_name}</p>`);
+        for (var j = 0; j < index.practices.length; j++) {
+          let practice = index.practices[j];
+          let address = practice.visit_address;
+          let site = practice.website;
+          $("#output").append(`<p class="address">${address.street}</p>`)
+          $("#output").append(`${address.city}, ${address.state}</p>`);
+          $("#output").append(`<p class="phone">${practice.phones[0].number}</p>`);
+          if(site === undefined) {
+            $("#output").append(`<p class="website">This doctor does not have a website</p>`);
+          } else {
+            $("#output").append(`<a href="${site}" class="website">${site}</a>`);
+          }
+          if(practice.accepts_new_patients === true) {
+            $("#output").append(`<p class="new-patient">Now accepting new patients</p>`);
+          } else {
+            $("#output").append(`<p class="new-patient">Not accepting new patients</p>`);
+          }
+        }
+      }
+      } else {
+        $("#output").append(`No doctors in the area fit match that inquiry.`)
+      }
+    }).fail(function(error) {
+      $("#output").text("Uh oh an error occurred");
     });
   });
 });
