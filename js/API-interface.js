e56f11b5fa9e6c37c7edcb5cd88c9be81596604e
Guest User <Guest@Epicodus-3D.local>
2017-12-05 16:34:09 -0800

    separate front end and back end logic

--- js/API-interface.js
+++ js/API-interface.js
@@ -1,76 +1,52 @@
-var apiKey = require('./../.env').apiKey;
+import {Call} from "./../js/API.js";
+
+
+var displayData = function(response) {
+  $("#output").show();
+  if(response.data.length > 0) {
+  for (var i = 0; i < response.data.length; i++) {
+    let index = response.data[i];
+    let profiles = index.profile;
+    $("#output").append(`<p class="name">${profiles.first_name} ${profiles.last_name}</p>`);
+    for (var j = 0; j < index.practices.length; j++) {
+      let practice = index.practices[j];
+      let address = practice.visit_address;
+      let site = practice.website;
+      $("#output").append(`<p class="address">${address.street}</p>`);
+      $("#output").append(`<p class="city">${address.city}, ${address.state}</p>`)
+      $("#output").append(`<p class="phone">${practice.phones[0].number}</p>`);
+      if(site === undefined) {
+        $("#output").append(`<p class="website">This doctor does not have a website</p>`);
+      } else {
+        $("#output").append(`<a href="${site}" class="website">${site}</a>`);
+      }
+      if(practice.accepts_new_patients === true) {
+        $("#output").append(`<p class="new-patient">Now accepting new patients</p>`);
+      } else {
+        $("#output").append(`<p class="new-patient">Not accepting new patients</p>`);
+      }
+    }
+  }
+  } else {
+    $("#output").append(`No doctors in the area fit match that inquiry.`)
+  }
+}
+
+var error = function(error) {
+  $("#output").text("Uh oh an error occurred");
+}
 
 $(document).ready(function() {
   // $("#output").hide();
+
+  var newCall = new Call();
   $("#ailment-button").click(function() {
     let input = $("#ailment").val();
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5231,-122.6765,100&user_location=45.5231,-122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response){
-      $("#output").show();
-      if(response.data.length > 0) {
-      for (var i = 0; i < response.data.length; i++) {
-        let index = response.data[i];
-        let profiles = index.profile;
-        $("#output").append(`<p class="name">${profiles.first_name} ${profiles.last_name}</p>`);
-        for (var j = 0; j < index.practices.length; j++) {
-          let practice = index.practices[j];
-          let address = practice.visit_address;
-          let site = practice.website;
-          $("#output").append(`<p class="address">${address.street}</p>`);
-          $("#output").append(`<p class="city">${address.city}, ${address.state}</p>`)
-          $("#output").append(`<p class="phone">${practice.phones[0].number}</p>`);
-          if(site === undefined) {
-            $("#output").append(`<p class="website">This doctor does not have a website</p>`);
-          } else {
-            $("#output").append(`<a href="${site}" class="website">${site}</a>`);
-          }
-          if(practice.accepts_new_patients === true) {
-            $("#output").append(`<p class="new-patient">Now accepting new patients</p>`);
-          } else {
-            $("#output").append(`<p class="new-patient">Not accepting new patients</p>`);
-          }
-        }
-      }
-      } else {
-        $("#output").append(`No doctors in the area fit match that inquiry.`)
-      }
-    }).fail(function(error) {
-      $("#output").text("Uh oh an error occurred");
-    });
+    newCall.getData(input, displayData)
   });
 
   $("#name-button").click(function() {
     let input = $("#name").val();
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${input}&location=45.5231,-122.6765,100&user_location=45.5231,-122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response){
-      $("#output").show();
-      if(response.data.length > 0) {
-      for (var i = 0; i < response.data.length; i++) {
-        let index = response.data[i];
-        let profiles = index.profile;
-        $("#output").append(`<p class="name">${profiles.first_name} ${profiles.last_name}</p>`);
-        for (var j = 0; j < index.practices.length; j++) {
-          let practice = index.practices[j];
-          let address = practice.visit_address;
-          let site = practice.website;
-          $("#output").append(`<p class="address">${address.street}</p>`)
-          $("#output").append(`${address.city}, ${address.state}</p>`);
-          $("#output").append(`<p class="phone">${practice.phones[0].number}</p>`);
-          if(site === undefined) {
-            $("#output").append(`<p class="website">This doctor does not have a website</p>`);
-          } else {
-            $("#output").append(`<a href="${site}" class="website">${site}</a>`);
-          }
-          if(practice.accepts_new_patients === true) {
-            $("#output").append(`<p class="new-patient">Now accepting new patients</p>`);
-          } else {
-            $("#output").append(`<p class="new-patient">Not accepting new patients</p>`);
-          }
-        }
-      }
-      } else {
-        $("#output").append(`No doctors in the area fit match that inquiry.`)
-      }
-    }).fail(function(error) {
-      $("#output").text("Uh oh an error occurred");
-    });
+    newCall.getData(input, displayData)
   });
 });
--- js/API.js
+++ js/API.js
@@ -1,1 +1,14 @@
+var apiKey = require('./../.env').apiKey;
 
+
+export class Call {
+  constructor(){
+  }
+  getData(response, displayData) {
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${response}&location=45.5231,-122.6765,100&user_location=45.5231,-122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response){
+      displayData(response)
+    }).fail(function(error) {
+      error(error);
+    });
+  }
+}
